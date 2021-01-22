17ed0b74c1a842df45ac4b8dd19d13b41901fef5
Serkan Özmen <ozmens@gmail.com>
2021-01-23 01:09:15 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> formalar = new MyDictionary<int, string>();
+            formalar.Add(13, "Atiba Hutchinson");
+            formalar.Add(14, "Vincent Aboubakar");
+            formalar.Add(24, "Domagoj Vida");
+            formalar.Add(10, "Sergen Yalçın");
+        }
+
+     
+    }
+}
